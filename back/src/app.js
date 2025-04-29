17909d8411f2e9267c1fc0409592ec78aeaec90f
FelipeUemura11 <felipe.uemura11@gmail.com>
2025-04-29 10:46:33 -0300

    frontend (react-ts/vite) instalado

--- back/src/app.js
+++ back/src/app.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3777;
 
 // Configuração do CORS para conexao com o frontend(5173)
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 app.use('/tarefa', TarefaRoutes);
---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import JOIAPEDROcopy from './assets/JOIAPEDROcopy.png'
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1>
+        Hello World! projetinho fellas!
+      </h1>
+      <img src={JOIAPEDROcopy} alt="JOIAPEDROcopy" />
+    </div>
+  )
+}
+
+export default App
